5670b526e5c281d32551d8107f59a8e43d1588a8
Martin N <mr.avgp@googlemail.com>
2012-05-22 19:03:58 +0200

    Adjusting mk_user_jail

--- mk_user_jail.sh
+++ mk_user_jail.sh
@@ -3,8 +3,13 @@
 CHROOT_PATH = "/tmp/chroot_$1"
 
 mkdir $CHROOT_PATH
+mkdir "$CHROOT_PATH/sys"
+mkdir "$CHROOT_PATH/proc"
+
 makejail "/tmp/chroot_conf_$1"
+
 cp -rfv /usr/share/php "$CHROOT_PATH/usr/share/"
 chown -R www-data:www-data $CHROOT_PATH
+
 mount -o bind "$CHROOT_PATH/sys" /sys
 mount -o bind "$CHROOT_PATH/proc" /proc
