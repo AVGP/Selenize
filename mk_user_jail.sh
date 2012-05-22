82ecab4694367b0ef81de63d7e22ac29e421cc2a
Martin N <mr.avgp@googlemail.com>
2012-05-23 01:01:45 +0200

    Correcting mount param order

--- mk_user_jail.sh
+++ mk_user_jail.sh
@@ -9,7 +9,8 @@ mkdir "$CHROOT_PATH/proc"
 makejail "/tmp/chroot_conf_$1"
 
 cp -rfv /usr/share/php "$CHROOT_PATH/usr/share/"
+cp /etc/hosts "$CHROOT_PATH/etc/"
 chown -R www-data:www-data $CHROOT_PATH
 
-mount -o bind "$CHROOT_PATH/sys" /sys
-mount -o bind "$CHROOT_PATH/proc" /proc
+mount -o bind /sys "$CHROOT_PATH/sys"
+mount -o bind /proc "$CHROOT_PATH/proc"
